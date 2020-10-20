32ad8f83a3caf68dcdbb11c1e3fd485b8fd6e811
jaguar6105 <gilljoseph603@gmail.com>
2020-10-20 16:43:48 -0400

    add question asking and simple readme generation

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# readmeGenerator
+readmeCreator
+    Table of Contents:
+    
+    Description: 
+    This application generates a professional looking readme
+    
+
+    Installation:
+    use node.js to run the application
+
+    Usage:
+    run the application in node.js then enter the information into the terminal when prompted
+
+    License: 
+    mit
+
+    Contributing:
+    me
+
+    Tests:
+    run
+
+    Questions:
+    My github is jaguar6105
+    Email me at gilljoseph603@gmail.com
+    
+    
+    
--- index.js
+++ index.js
@@ -3,19 +3,95 @@ const util = require("util");
 const inquirer = require("inquirer");
 
 
-// array of questions for user
-const questions = [
+/*
+* The generated README includes the following sections: 
 
+  * Title
+  * Description
+  * Table of Contents
+  * Installation
+  * Usage
+  * License
+  * Contributing
+  * Tests
+  * Questions
+
+* The generated README includes 1 badge that's specific to the repository.
+
+*/
+// array of questions for user
+const questions = ["Title: ",
+"Description: ",
+"Installation: ",
+"Usage: ",
+"License: ",
+"Contributing: ",
+"Test Instructions: ",
+"Github UserName: ",
+"Email: "
 ];
+
+const answers = [];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
 }
 
 // function to initialize program
-function init() {
+async function init() {
+    for(let i = 0; i < questions.length; i++){
 
+    await askQuestion(questions[i]);
+    }
+    writeToFile("README.md", generateReadmeText());
 }
 
-// function call to initialize program
-init();
+
+async function askQuestion(question) {
+    try {
+        //for(let i = 0; i < questions.length; i++){
+        const { answer } = await inquirer.prompt({
+            message: question,
+            name: "answer"
+        })
+        answers.push(answer);
+    //}
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function generateReadmeText() {
+    const text = `${answers[0]}
+    Table of Contents:
+    
+    Description: 
+    ${answers[1]}
+    
+
+    Installation:
+    ${answers[2]}
+
+    Usage:
+    ${answers[3]}
+
+    License: 
+    ${answers[4]}
+
+    Contributing:
+    ${answers[5]}
+
+    Tests:
+    ${answers[6]}
+
+    Questions:
+    My github is ${answers[7]}
+    Email me at ${answers[8]}
+    
+    
+    `
+    return text;
+}
+    // function call to initialize program
+    init();
